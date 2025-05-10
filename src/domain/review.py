ad14a69d82598a5635763684a844ed914f459c63
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-05-10 14:42:14 +0900

    chore: #3 Added review entity

---
+++ src/domain/review.py
@@ -0,0 +1,14 @@
+﻿from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class Review:
+    id: int
+    datePosted: datetime
+    isRecommended: bool
+    hoursPlayed: float
+
+    def __post_init__(self):
+        if isinstance(self.datePosted, str):
+            self.datePosted = datetime.strptime(self.datePosted, "%Y-%m-%d")
