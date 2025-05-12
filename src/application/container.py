efd88f0b919f048ec7ede3735fa3657d6714b549
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-05-12 12:39:55 +0900

    chore: #3 Added MapReduce service

--- src/application/container.py
+++ src/application/container.py
@@ -4,8 +4,10 @@ from dependency_injector import containers, providers
 
 from src.application.common import Logger
 from src.application.services.hadoop_service.hadooop_service import HadoopService
+from src.application.services.mapreduce_service import MapreduceService
 
 
 class Container(containers.DeclarativeContainer):
     logger = providers.Singleton(Logger.get_logger, name="Hadoop", level=logging.INFO)
     hadoop_service = providers.Singleton(HadoopService, logger=logger)
+    mapreduce_service = providers.Singleton(MapreduceService, logger=logger)
---
+++ src/application/services/mapreduce_service/mapreduce_service.py
@@ -0,0 +1,40 @@
+﻿import logging
+import os
+import subprocess
+import time
+
+from src import Config
+
+
+class MapreduceService:
+    __logger: logging.Logger
+
+    def __init__(self, logger: logging.Logger):
+        self.__logger = logger
+
+    def run_mapreduce_subprocess(self, filename: str) -> None:
+        self.__logger.info("Running MapReduce job via Hadoop streaming...")
+        start_time = time.time()
+        subprocess.run(Config.HDFS_STREAMING_COMMAND.value, check=True)
+        elapsed_time = round((time.time() - start_time) / 60, 2)
+        self.__logger.info(f"MapReduce job completed in {elapsed_time} minutes")
+
+    def get_mapreduce_result(self) -> dict[str, tuple[float, float]]:
+        self.__logger.info("Displaying results from MapReduce job")
+        subprocess_result = subprocess.run(
+            ["hadoop", "fs", "-cat", os.path.join(Config.HDFS_OUTPUT_PATH.value, "part-*")],
+            text=True,
+            capture_output=True
+        )
+
+        result = {}
+        lines = subprocess_result.stdout.strip().split("\n")
+
+        for line in lines:
+            time_group, recommended = line.split()
+            recommended = float(recommended)
+            not_recommended = float(0)  # TODO: Updated this when MapReduce job is updated
+
+            result[time_group] = (recommended, not_recommended)
+
+        return result
