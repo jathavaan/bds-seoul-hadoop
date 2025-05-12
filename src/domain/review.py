5acd42eae3f90f2ec695f92efedf1911d4cf58da
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-05-12 15:48:25 +0900

    chore: #3 Added file service

--- src/domain/review.py
+++ src/domain/review.py
@@ -4,11 +4,15 @@ from datetime import datetime
 
 @dataclass
 class Review:
-    id: int
+    game_id: int
     datePosted: datetime
     isRecommended: bool
     hoursPlayed: float
+    user_id: int
 
     def __post_init__(self):
         if isinstance(self.datePosted, str):
             self.datePosted = datetime.strptime(self.datePosted, "%Y-%m-%d")
+
+    def __repr__(self):
+        return f"{self.game_id},{self.datePosted},{1 if self.isRecommended else 0},{self.hoursPlayed},{self.user_id}"
