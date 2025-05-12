9ed9aa36f07846efc86fbf3db76469c8b12595a8
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-05-12 20:37:37 +0900

    chore: #3 Added fileservice as a dependency

--- src/application/container.py
+++ src/application/container.py
@@ -3,11 +3,13 @@
 from dependency_injector import containers, providers
 
 from src.application.common import Logger
-from src.application.services.hadoop_service.hadooop_service import HadoopService
+from src.application.services.hadoop_service.hdfs_service import HdfsService
 from src.application.services.mapreduce_service import MapreduceService
+from src.application.services.file_service import FileService
 
 
 class Container(containers.DeclarativeContainer):
     logger = providers.Singleton(Logger.get_logger, name="Hadoop", level=logging.INFO)
-    hadoop_service = providers.Singleton(HadoopService, logger=logger)
+    hdfs_service = providers.Singleton(HdfsService, logger=logger)
     mapreduce_service = providers.Singleton(MapreduceService, logger=logger)
+    file_service = providers.Singleton(FileService, logger=logger)
