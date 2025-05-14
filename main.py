bfbe63f6efd72bea83ac13534f5f5b5a8ae8318f
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-05-14 13:12:19 +0900

    chore: #3 Removed unused code

--- main.py
+++ main.py
@@ -1,48 +1,7 @@
-import subprocess
-import time
-
-from hdfs import InsecureClient, HdfsError
-
-from src import Config
 from src.entrypoints.consumers.review_consumer import ReviewConsumer
 from src.entrypoints.producers import MapreduceReduceResultProducer
 
 if __name__ == "__main__":
-    start_time = time.time()
-    print("Sleeping for 30 seconds... Waiting for Hadoop to finish setup")
-    time.sleep(Config.HDFS_SETUP_TIMEOUT.value)
-
-    max_retries = Config.HDFS_CONNECT_MAX_RETRIES.value
-    client = InsecureClient(url=Config.HDFS_CONNECTION_STRING.value, user=Config.HDFS_USER.value)
-
-    for i in range(max_retries):
-        try:
-            client.status("/")
-            print("Successfully connected to namenode")
-            break
-        except HdfsError as e:
-            print(f"Waiting for namenode ({i + 1} / {max_retries})\n\n{e}")
-    else:
-        raise RuntimeError("Namenode did not become available")
-
-    try:
-        status = client.status(Config.HDFS_INPUT_PATH.value)
-
-        if status["type"] == "DIRECTORY":
-            print(f"Directory {Config.HDFS_INPUT_PATH.value} already exists")
-    except HdfsError:
-        print(f"Directory {Config.HDFS_INPUT_PATH.value} does not exist, creating...")
-        client.makedirs(Config.HDFS_INPUT_PATH.value)
-
-    while True:
-        result = subprocess.run(["hdfs", "dfsadmin", "-safemode", "get"], capture_output=True, text=True)
-        if "Safe mode is OFF" in result.stdout:
-            print("HDFS is out of safe mode. Proceeding...")
-            break
-        else:
-            print("HDFS is in safe mode. Waiting...")
-            time.sleep(5)
-
     # file_to_upload = os.path.join(os.getcwd(), "airline.csv")
     #
     # print(f"Uploading {file_to_upload} to HDFS...")
@@ -66,10 +25,6 @@ if __name__ == "__main__":
     # )
     #
     # print(result.stdout)
-
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-
     consumer = ReviewConsumer()
     producer = MapreduceReduceResultProducer()
 
@@ -82,4 +37,3 @@ if __name__ == "__main__":
     finally:
         consumer.close()
         producer.close()
-        print(f"Runtime: {elapsed_time / 60:.2f} minutes")
