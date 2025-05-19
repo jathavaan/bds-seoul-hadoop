4e5674b175d738c47986176fcce266cf54951ac8
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-05-19 10:48:08 +0900

    chore: #3 Code cleanup

--- main.py
+++ main.py
@@ -4,29 +4,6 @@ from src.entrypoints.consumers.review_consumer import ReviewConsumer
 from src.entrypoints.producers import MapreduceReduceResultProducer
 
 if __name__ == "__main__":
-    # file_to_upload = os.path.join(os.getcwd(), "airline.csv")
-    #
-    # print(f"Uploading {file_to_upload} to HDFS...")
-    # client.upload(Config.HDFS_INPUT_PATH.value, file_to_upload, overwrite=True)
-    # print(f"File uploaded: {client.content(Config.HDFS_INPUT_PATH.value)}")
-    #
-    # print("Cleaning up old HDFS output directory (if any)...")
-    # subprocess.run([
-    #     "hadoop", "fs", "-rm", "-r", Config.HDFS_OUTPUT_PATH.value
-    # ], stderr=subprocess.DEVNULL)
-    #
-    # print("Running MapReduce job via Hadoop streaming...")
-    # subprocess.run(Config.HDFS_STREAMING_COMMAND.value, check=True)
-    # print("MapReduce job completed.")
-    #
-    # print("Fetching and displaying output from HDFS...")
-    # result = subprocess.run(
-    #     ["hadoop", "fs", "-cat", os.path.join(Config.HDFS_OUTPUT_PATH.value, "part-*")],
-    #     text=True,
-    #     capture_output=True
-    # )
-    #
-    # print(result.stdout)
     consumer = ReviewConsumer()
     producer = MapreduceReduceResultProducer()
 
